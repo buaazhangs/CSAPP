3333efcc33411a97102def255dbed7a423ec6faf
Zhang <286666107@qq.com>
2023-11-23 22:17:35 +0800

    20231123

---
+++ 剩余课程文件夹/四、程序-系统交互/系统交互示例代码/Fork.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+//错误信息
+void unix_Error(char *msg){
+    fprintf(stderr,"%s\n",msg);
+    exit(0);
+}
+//fork包装
+pid_t Fork(void){
+    pid_t pid = fork();
+    if (pid < 0)
+        unix_Error("fork error");
+    return pid;
+}
+
+int main(){
+    __pid_t pid;
+    int x = 1;
+    pid = Fork();
+    if (pid == 0){
+        printf("child process:%d\n",++x);
+        exit(0);
+    }
+    printf("father process:%d\n",--x);
+    exit(0);
+}
