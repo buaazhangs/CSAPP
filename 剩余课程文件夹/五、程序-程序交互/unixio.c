643ab4c347792625ac9186d0a2173a572e48002f
Zhang <286666107@qq.com>
2024-01-22 21:33:16 +0800

    20240122

--- 剩余课程文件夹/五、程序-程序交互/unixio.c
+++ 剩余课程文件夹/五、程序-程序交互/unixio.c
@@ -1,1 +1,11 @@
-#include<csapp.h>
+//#include "csapp.h"
+#include "csapp.h"
+#include <stdlib.h>
+
+int main(){
+    int fd1,fd2;
+    fd1 = open("markdown.md",O_RDONLY,0);
+    printf("fd1 = %d\n",fd1);
+    close(fd1);
+    exit(0);
+}
